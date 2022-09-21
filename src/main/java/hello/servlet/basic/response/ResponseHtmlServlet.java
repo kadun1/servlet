d593ac3bfc912d15181cc4ec9e14aefc809a41c3
kadun1 <kadunjj@gmail.com>
2022-09-21 23:01:55 +0900

    Spring-Study request & response

---
+++ src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,8 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+}
